97b4532c42e7f5fa46f737fc433ceaec886de2fe
Gia1987 <giacomo.n27@gmail.com>
2018-03-23 12:48:46 +0000

    Add 'APIs' to the thermostat

--- src/interface.js
+++ src/interface.js
@@ -36,4 +36,22 @@ $(document).ready(function(){
   $('#power-saving-status').text(thermostat.savingMode());
   };
 
+ displayWeather('london')
+
+  $('#select-city').submit(function(event){
+    event.preventDefault();
+    var city = $('#city').val();
+    displayWeather(city);
+  })
+
+  function displayWeather(city){
+    var url ='http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get( url + token + units, function(data) {
+      $('#cityname').text(data.name + ": ");
+      $('#outsidetemperature').text(data.main.temp).append(' &#8451;');
+    });
+  }
+
 });
